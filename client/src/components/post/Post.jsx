80c35a6d839c998ce758a9908a96d8f36f0fb0cb
SinghJashanpreet <jashanpreet6081@gmail.com>
2023-09-11 13:37:26 +0530

    trt

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -2,7 +2,7 @@ import "./post.css";
 import { Link } from "react-router-dom";
 
 export default function Post({ post }) {
-  const PF = "http://https://blogverse-iy7h.onrender.com/images/";
+  const PF = "https://blogverse-iy7h.onrender.com/images/";
   return (
     <div className="post">
       {post.photo && <img className="postImg" src={PF + post.photo} alt="" />}
